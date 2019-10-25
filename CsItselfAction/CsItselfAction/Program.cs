e67f0ffd89b2dce1e8c6eafb934b93bb9a21b25e
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-25 15:52:47 +0300

    Added examples

--- CsItselfAction/CsItselfAction/Program.cs
+++ CsItselfAction/CsItselfAction/Program.cs
@@ -10,11 +10,31 @@ namespace CsItselfAction
         {
             Print print = ConsolePrint;
             print.Invoke(1234);
+
+            Action<int> printActionDel = ConsolePrint;
+            printActionDel(200);
+
+            Action<string> printMessage = ConsolePrint;
+            printMessage.Invoke("Hello, world!");
+
+            Action<string, int> printLongMessage = new Action<string, int>(ConsolePrint);
+            printLongMessage.Invoke("Error", 404);
+
         }
 
         static void ConsolePrint(int i)
         {
             Console.WriteLine(i);
+        }
+
+        static void ConsolePrint(string message)
+        {
+            Console.WriteLine("Message : " + message);
+        }
+
+        static void ConsolePrint(string message, int value)
+        {
+            Console.WriteLine($"{message} #{value}");
         }
     }
 }
