d476282ee583413a07eafa36a2c245586780b79d
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-25 15:45:34 +0300

    Added project

---
+++ CsItselfAction/CsItselfAction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CsItselfAction
+{
+    class Program
+    {
+        public delegate void Print(int val);
+
+        static void Main(string[] args)
+        {
+            Print print = ConsolePrint;
+            print.Invoke(1234);
+        }
+
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
